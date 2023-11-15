4fe38c5af43fad611b931938dad56838018e8db0
ogzCode <oguzguc44@gmail.com>
2023-11-15 08:42:14 +0300

    Add login stack to navigation

--- react-native-note/src/navigation/Navigation.js
+++ react-native-note/src/navigation/Navigation.js
@@ -1,11 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabStack from "./stack/bottom/BottomTabStack";
+import LoginStack from "./stack/login/LoginStack";
 import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <BottomTabStack />
+            <Stack.Navigator screenOptions={{
+                gestureEnabled: true,
+                headerShown: false
+            }}>
+                <Stack.Screen name="LoginStack" component={LoginStack} />
+                <Stack.Screen name="BottomTabStack" component={BottomTabStack} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
